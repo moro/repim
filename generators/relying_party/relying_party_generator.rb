ccf1c428912dbd3f4c7464b87c48ceda9353c8b6
moro <moronatural@gmail.com>
2009-03-09 16:47:41 +0900

    fixup generator.

--- generators/relying_party/relying_party_generator.rb
+++ generators/relying_party/relying_party_generator.rb
@@ -4,10 +4,23 @@ class RelyingPartyGenerator < Rails::Generator::NamedBase
     user_name, user_cols = @args
 
     if user_name
-      @user_klass_name = user_name.singularize.under_score
+      @user_klass_name = user_name.singularize.underscore
       @user_cols = user_cols
     else
       @user_klass_name = "user"
+    end
+  end
+
+  def add_options!(opt)
+    opt.on("--skip-user-scaffold",
+           "Create user model with (rspec_)model generator. In default we user (rspec_)scaffold."){|v| options[:skip_user_scaffold] = true }
+
+    opt.on("--skip-plugins-spec",
+           "Do'nt copy application_controller_spec.rb and #{plural_name}_controller_spec.rb, default is #{!using_rspec?}"){|v| options[:skip_sessions_spec] = true }
+
+    opt.on("--openid-migration=migration_name",
+           "Specify openid migration name, default is 'open_id_authentication_tables'. If value is 'none' generate nothing.") do |v|
+      options[:openid_migration] = v
     end
   end
 
@@ -18,27 +31,50 @@ class RelyingPartyGenerator < Rails::Generator::NamedBase
     record do |m|
       m.file "sessions_controller.rb", "app/controllers/#{controller_file_name}.rb"
       m.route_resources plural_name
-      m.dependency("open_id_authentication_tables", ["open_id_authentication_tables" ])
 
-      model_generator = using_rspec? ? "rspec_model" : "model"
-      m.dependency(model_generator, [@user_klass_name, "identity_url:string", user_cols].flatten.compact)
+      unless options[:skip_sessions_spec]
+        m.directory "spec/controllers"
+        m.file "spec/application_controller_spec.rb", "spec/controllers/application_controller_spec.rb"
+        m.file "spec/sessions_controller_spec.rb", "spec/controllers/#{controller_file_name}_spec.rb"
+      end
 
-      if $0 =~ %r!script/destroy!
-        m.gsub_file('app/controllers/application.rb', /(^\s*include Repim::Application\s*$)/mi){|m| "" }
-      else
-        m.gsub_file 'app/controllers/application.rb', /(^\s*helper\s.*?$)/mi do |match|
-          "#{match}\n  include Repim::Application"
+      m.dependency(care_rspec(options[:skip_user_scaffold] ? "model" : "scaffold"),
+                   [@user_klass_name, "identity_url:string", @user_cols].flatten.compact)
+
+      # FIXME very veriy dirty
+      # "sleep 3" is for changing timestamp of 'create_user' or 'open_id_authentication_tables'
+      if options[:command] == :create
+        m.gsub_file(app_controller, /(^\s*helper\s.*?$)/mi) do |ms|
+          sleep 3
+          "#{ms}\n  include Repim::Application"
         end
+      else
+        m.gsub_file(app_controller, /(^\s*include Repim::Application\s*$)/mi){|m| "" }
+      end
+
+      unless options[:openid_migration] == "none"
+        name = options[:openid_migration] || "open_id_authentication_tables"
+        m.dependency("open_id_authentication_tables", [name])
       end
     end
   end
 
   private
+  def app_controller
+    %w[application_controller application].
+      map{|f| "app/controllers/#{f}.rb" }.
+      detect{|c| File.exists?(File.expand_path(c, RAILS_ROOT)) }
+  end
+
   def user_model_exists?
     File.exists?( File.expand_path(@user_klass_name + ".rb", RAILS_ROOT + "/app/models") )
   end
 
+  def care_rspec(base)
+     using_rspec? ? "rspec_#{base}" : base
+  end
+
   def using_rspec?
-    File.directory?( File.expand_path("spec"), RAILS_ROOT )
+    File.directory?( File.expand_path("spec", RAILS_ROOT) )
   end
 end
