d4f4ac039721c5d253a74004f722c358f1bcf707
moro <moronatural@gmail.com>
2009-03-09 15:32:19 +0900

    fix test

--- integration/sample-app/spec/controllers/application_controller_spec.rb
+++ integration/sample-app/spec/controllers/application_controller_spec.rb
@@ -7,7 +7,7 @@ describe ApplicationController do
   describe "#logged in" do
     before do
       session[:user_id] = 12345
-      ApplicationController.user_klass.should_receive(:find).with(12345).and_return(@user = mock("user"))
+      ApplicationController.user_klass.should_receive(:find_by_id).with(12345).and_return(@user = mock("user"))
     end
 
     it{ controller.should be_signed_in }
